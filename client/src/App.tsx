c24a80bed238cf8776a0569c12c0d258bc2efcdb
abelgtzrs <32408235+abelgtzrs@users.noreply.github.com>
2025-04-17 17:42:09 -0400

    fixed import issues

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,47 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+// client/src/App.tsx
+import "./App.css";
+import { Outlet } from "react-router-dom";
 
-import Navbar from './components/Navbar';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
 
+import { setContext } from "@apollo/client/link/context";
+
+import Navbar from "./components/Navbar";
+
+// 1. HTTP Link to /graphql
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// 2. Auth header using token from localStorage
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// 3. Create Apollo Client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+// 4. Wrap your app
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
