31dc5ebbcc4ff9620ea866600ef81ca5ff7f8476
abelgtzrs <32408235+abelgtzrs@users.noreply.github.com>
2025-04-18 22:09:04 -0400

    Fixed __dirname error for ERM compatibility

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,9 +6,14 @@ import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { typeDefs, resolvers } from "./schemas/index.js";
 import { authMiddleware } from "./services/auth.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const app: Application = express();
 const PORT = process.env.PORT || 3001;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); // or '*'
